803d4f5a6e2d9905f47ec9d596b0441050b167cb
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2025-09-24 15:36:11 +0000

    ts file is add

---
+++ pro.ts
@@ -0,0 +1,13 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const user: User = {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@gmail.com"
+};
+
+console.log(user);
